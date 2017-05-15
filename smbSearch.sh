36e3b4fc6ad3bc17c84ad984a7ee7d8155bea7a8
someone25572 <someone25572@mail2tor.com>
2017-05-15 10:55:39 +0200

    ip print edit

--- smbSearch.sh
+++ smbSearch.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash  #you know what this is
 for i in $(shuf smb); do
-	proxychains smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP
+	echo $i		#print ip
+	proxychains smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP #get basic infos
 
 for o in $(cat DISK_TEMP) ; do
-	proxychains smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list
+	proxychains smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list #get file grepable output
 done
 done
-rm -fv DISK_TEMP
+rm -fv DISK_TEMP #cleaning
