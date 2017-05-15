5250a1cc224bdc6d10d23557213b59b5b8300574
kernelzeroday <kod@localhost>
2017-05-15 09:05:13 +0000

    clean style

--- smbSearch.sh
+++ smbSearch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash  #you know what this is
+
 for i in $(shuf smb); do
+
 	smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP
 
-for o in $(cat DISK_TEMP) ; do
-	smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list
+	for o in $(cat DISK_TEMP) ; do
+
+		smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list
+
+	done
+
 done
-done
+
 rm -fv DISK_TEMP #cleaning
