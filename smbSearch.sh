6e5c2f991a8f8bc8b07d5d3e155d43ce1cac3ed3
someone25572 <someone25572@mail2tor.com>
2017-05-15 11:09:50 +0200

    fix

--- smbSearch.sh
+++ smbSearch.sh
@@ -2,11 +2,13 @@
 
 for i in $(shuf smb); do
 
-	smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP
+	echo $i #print ip
+
+	smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP #get general info
 
 	for o in $(cat DISK_TEMP) ; do
 
-		smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list
+		smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list #get file lists
 
 	done
 
