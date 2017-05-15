d8ad608bdb0b0aa33a47d54346037fc96cf08d6d
someone25572 <someone25572@mail2tor.com>
2017-05-15 10:19:59 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in $(shuf smb); do
-	proxychains smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr-s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP
+	proxychains smbclient -g -N -L "\\\\$i\\"  | grep Disk | tr -s ' ' | sed 's/^ //g' | cut -f1 -d' ' > DISK_TEMP
 
 for o in $(cat DISK_TEMP) ; do
 	proxychains smbclient -N -g "\\\\$i\\$o" -c "recurse;dir"  | sed "s/^/$i:$o /g" | tr -s ' '  | tee -a file.list
