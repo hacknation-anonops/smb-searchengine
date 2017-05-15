38ab05eb60020e43287e49ab5200403906d3523b
someone25572 <someone25572@mail2tor.com>
2017-05-15 23:27:12 +0200

    update

--- smbSearch.sh
+++ smbSearch.sh
@@ -18,4 +18,8 @@ rm -fv DISK_TEMP #cleaning
 
 sh analysis.sh #doing a quick one
 
-smbclient -N -g "\\\\$(cat out.file |awk '{print $1}' |tr -d 'IP=')\\$(cat out.file |awk '{print $2}' |tr -d 'SHARE=')\$(cat out.file |awk '{print $3}' |tr -d 'PATH=')" -c "get"
+for line in $(shuf out.final.file); do
+
+	smbclient -N -g "\\\\$(echo $line |awk '{print $1}' |tr -d 'IP=')\\$(echo $line |awk '{print $2}' |tr -d 'SHARE=')\$(echo $line |awk '{print $3}' |tr -d 'PATH=')" -c "get"
+
+done
