6b09d51d05d5b716ee1c43987a6d789037b8fa36
someone25572 <someone25572@mail2tor.com>
2017-05-15 22:31:59 +0200

    for your bash

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
+outfile = open("out.file", "w")
 for line in open("file.list", "r", encoding="utf-8"):
     line = line.split(" ", maxsplit=2)
     if line[2].startswith('\\'):
-        print(line)
+        if line[2].endswith("\n"):
+            line[2] = line[2].split("\n")[0]
+        outfile.writelines("IP=" + str(line[0] + " " + "SHARE=" + str(line[1]) + " " + "PATH=" + str(line[2]) + "\n"))
