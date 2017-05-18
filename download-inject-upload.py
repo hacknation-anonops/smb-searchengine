482fa858c7cd3d252445687c2364124027cd8f79
someone25572 <someone25572@mail2tor.com>
2017-05-18 14:03:45 +0200

    clean up1

--- download-inject-upload.py
+++
@@ -1,8 +0,0 @@
-import smb.SMBConnection
-
-# for IP in open("ips"):
-IP = "192.168.1.120"
-host = smb.SMBConnection.SMBConnection("", "", "SOMEFUCK", IP)
-host.connect(IP, port=445, timeout=80)
-for SHARE in host.listShares(timeout=80):
-    print(host.listPath(SHARE, "Musik"))
